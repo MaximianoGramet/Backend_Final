8677f762044222e5549af9473ed6281e6a9c059a
Gramet Maximiliano <maxigramet03@gmail.com>
2024-05-07 19:15:40 -0300

    cors test

--- src/config/server/express.config.js
+++ src/config/server/express.config.js
@@ -19,12 +19,12 @@ expressApp.use(express.json());
 expressApp.use(express.urlencoded({ extended: true }));
 expressApp.use(
   cors({
-    //origin: FRONT_URL,
-    methods: ["GET", "POST", "DELETE", "PUT", "HEAD"],
-    credentials: true,
-    allowedHeaders:
-      "Content-Type,Access-Control-Allow-Origin, Authorization, X-Request-With, Accept, Origin, Access-Control-Allow-Headers",
-    optionsSuccessStatus: 204,
+    // origin: FRONT_URL,
+    // methods: ["GET", "POST", "DELETE", "PUT", "HEAD"],
+    // credentials: true,
+    // allowedHeaders:
+    //   "Content-Type,Access-Control-Allow-Origin, Authorization, X-Request-With, Accept, Origin, Access-Control-Allow-Headers",
+    // optionsSuccessStatus: 204,
   })
 );
 
